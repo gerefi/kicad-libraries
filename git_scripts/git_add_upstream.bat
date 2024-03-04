6178e014dae56af834baefb88ef0338c9e4ef9e0
Bruno Ferreira <c_motorsport@hotmail.com>
2024-03-04 21:53:05 +0100

    Library

---
+++ git_scripts/git_add_upstream.bat
@@ -0,0 +1,6 @@
+echo Showing the status prior to adding upstead
+git remote -v
+echo Trying to add 'official_DELTA-Pro_upsteam' 
+git remote add official_DELTA-Pro_upstream https://github.com/DELTA-Pro/kicad-libraries.git
+echo Showing the status after adding upstead
+git remote -v
---
+++ git_scripts/git_reset_to_upstream.bat
@@ -0,0 +1,4 @@
+git fetch official_DELTA-Pro_upstream
+git checkout master
+git reset --hard official_DELTA-Pro_upstream/master
+git push origin master -f
