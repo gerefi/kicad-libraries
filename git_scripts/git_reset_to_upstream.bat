99cc09bf8d54959b3af260b1396c1e1443356553
Bruno Ferreira <c_motorsport@hotmail.com>
2024-03-10 15:49:47 +0100

    new

--- git_scripts/git_reset_to_upstream.bat
+++ git_scripts/git_reset_to_upstream.bat
@@ -1,4 +1,4 @@
-git fetch official_DELTA-Pro_upstream
+git fetch official_gerefi_upstream
 git checkout master
-git reset --hard official_DELTA-Pro_upstream/master
+git reset --hard official_gerefi_upstream/master
 git push origin master -f
